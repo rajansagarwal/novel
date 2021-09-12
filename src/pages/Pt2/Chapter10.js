e5d5b7df920e5f2d8fded45690dcd42838976e79
Rajan Agarwal <rajan.ag005@gmail.com>
2021-09-12 15:13:40 -0400

    10 chapters completed

--- src/pages/Pt2/Chapter10.js
+++ src/pages/Pt2/Chapter10.js
@@ -30,47 +30,84 @@ export default function Chapter1() {
             <Col sm={2}></Col>
             <Col sm={8}>
               <p className="p-manifesto">
-                "Hey. Wake up"<br/><br/>
-Slowly, I open my eyes, only to see my dad smiling over my bed. As I fall back into a deep stretch, I look at the time. 7:03 AM. <br/><br/>
-    "Dad. It's the weekend. Why so early-"<br/> 
-I get cut off by a deep yawn, lasting what felt like hours. My dad breaks the sleepy environment with his enthusiastic tone. <br/><br/>
-      "Let's go Ryan. Get up. You won't regret it."<br/><br/>
-Gradually, I lift myself out of bed. I hear the birds chirping like a summer melody, the sound of wind in the rustling trees. I open my window and feel the fresh air blowing in my face. After waking myself up, I walk over to the kitchen, heading straight for the cupboard. As I pour myself a quick bowl of cereal, I await the reasoning for this early excitement with my father. My brother, Bobby, is waiting on the couch, quite confused as well.<br/><br/>
-    "Well, boys. I got a little something. Now, I know it will be a bit of work, but I know you will love it!"<br/><br/>
-    {'  '}"What is it! What is it! What is it!" Bobby exclaims. <br/>{'  '}
-In a tired and anxious voice, I reply, "Someone's finally awake."<br/><br/>
-My dad, filled with excitement, bends down to get something.<br/>
-    "Introducing our new little buddy."<br/><br/>
-He picks up a soft little brown and black puppy. This is, by far, the cutest thing I have ever seen. The tiny little paws are waving in the air—the small little wet nose. The tiny ears are flopping around. The tiny tail wagging uncontrollably. <br/><br/>Without hesitation, my brother yells, "Oh. My. God. It's a dog!"
-    "Can I touch it? Can I hold him? Can I have him?"<br/>
-    "Yes, yes, you can.” <br/><br/>
-Myself, I’m still processing this. I mean, they know that I can't take care of him myself. I take a very long pause, trying to recollect my thoughts. I look over at Bobby, playing and running around with the puppy. He picks him up and cradles him like a newborn baby. Seeing Bobby like this, I knew I couldn't do this to him. He hasn't been this happy in quite a long time.
-    "How are we supposed to take care of him?"<br/><br/>
-    Dad looks over. The mood shifts quickly with this blunt statement.
-"Well," I take another pause. "Of course, Bobby is young, so that will be difficult. With mom gone as well, I don’t see how this would be possible.”<br/><br/>
-We share a few pauses. Back and forth, more thoughts flow by the second.
-    "And of course, you know I can't."<br/><br/>
-   "You are highly functional and will be very successful! Don't tell yourself that!”<br/>
-    “Just as a recap, you are leaving an autistic boy and a child alone with a dog?”<br/><br/>
-The pauses become longer with each thought.
-    "But on a more important note, how did you afford this dog? He seems too beautiful to be free."<br/><br/>
-    "That is a question I do know the answer to. A coworker from my old job got one, but after a couple of weeks, it was too much to handle."<br/><br/>
-    "And you don't think that says something for itself."<br/><br/>
-    "Well, it was such a beautiful dog that I had to take in."<br/><br/>
-Bobby overhears a few words and chimes in, "Before making a verdict, come hold him."<br/><br/>
-    My dad looks at him, confused.
-    "Where did you learn that word?"<br/><br/>
-    "I saw it on a commercial. But that doesn't matter. Just come!"<br/><br/>
-Very slowly, I walk over to the couch. When I say slowly, I mean exceptionally slowly. My hands are shaking, and my thoughts are going crazy. What if it jumps? What if it barks? What if it bites? 
-    "Nope. This is definitely not happening"<br/><br/>
-I run back. Well, walk quickly, but it's the thought that counts. My dad blocks me and whispers into my ear. 
-    "Leap of faith."<br/><br/>
-Even though I am in no mood to listen to his advice, I turn around, walk to the couch and sit down. 
-    "Alright," I took a very long pause. I'm ready."<br/><br/>
-Bobby slowly takes the puppy and places him on my lap. Well, what do you know? Nothing happened. I gently bring up my hands and stroke his fur very gently. A feeling of positive energy flows through me. I look over to my brother and dad and then back at the dog.
-<br/><br/>
-"Welcome to the family."<br/>
-             <br/>
+              It's my first day and to be completely honest, I am excited. Today, I finally get to be in the mainstream program full-time. I have been waiting for three months, so this is a big deal to me. Over the summer, I prepared to get the supplies and resources I need. A new pencil case, pencils, erasers, pens, notebooks, lunch box and the list, goes on. I don't want any surprises for today.<br/><br/>
+    I'm a bit nervous, though. I'm not expecting too much, but I don't know what to expect in the first place. Yeah, I went to that one class before, but it's not the same. Whenever my dad talks about it, he tells me, 'just be yourself. I've never really understood what that phrase means? From what I see around me, people change who they are to create a good impression. No one is ever themselves. I don't talk much, though, so this should be fine. I quickly look at the time. 7:30 AM. I don't want to be late.<br/><br/>
+    "It's time to go."<br/>
+    "Be right there, son!"<br/><br/>
+The three (well four) of us get in the car and start heading over to the school. Not sure why but Bobby insisted on coming for my first day. It's his first day too! He tries to break the silence in the hopes of a more joyful car ride.<br/><br/>
+    "So Ryan… you excited?!"<br/>
+I nod quickly.<br/>
+    "I'm glad. You should be! What are your courses again."<br/>
+    "Math. Chemistry. English. Political Science"<br/>
+    "Ooh. Which are you most excited for?"<br/>
+    "Chemistry"<br/>
+    "Hmmm"<br/><br/>
+    "Are you excited, Bobby?"<br/>
+He looks a little shocked. I usually don't talk on car rides, but today is my first day of real high school, so I guess anything can happen. <br/>
+    "Yeah! A new school means new friends!"<br/>
+I give him a short smile, which is then quickly returned by both Bobby and Dad. When I look outside, I'm pretty surprised that we have already arrived. The school looks the same but feels different. I can already tell it's going to be a good day. As we pull into the school, we become stuck behind a parade of cars. You might as well wait.<br/><br/>
+    Finally, our car makes it to the front of the line, where I grab my stuff. I take a deep breath and fix my glasses. <br/><br/>
+    "Hey, son. Before you go, just know that no matter what, I love you, and I'm so proud of you."<br/><br/>
+    After a quick smile, I head out of the car and to the front doors. I pull out my itinerary and check the class numbers. I head on over to door 6B and stand in front of it. These next few steps are going to be the beginning of a new journey. Just as I am about to take my next steps, someone accidentally runs into me and shoves me to the side. Is this really how it's going to be? Where's the civilized behaviour? 
+    After a quick recovery, I head inside, only to find the loudest jungle I've ever seen. Noises coming left and right, music playing extremely loud on the overhead speakers. <br/><br/>My class is in room 124, just around the corner. I guess it's nice that my style is so close to the doors. Less time spent in this mess they call a school. If I must say, this has not been the best start.
+    As I finally arrive at my class, I take another deep breath. I love math, so this should be fine. Except for the fact that I'm going to be in a class next to a bunch of primitive high schoolers. I walk into the classroom, only to be introduced to the teacher, Ms. Miller. <br/><br/>
+    "Hi, there! I'm Ms. Miller, your math teacher. And you are?"<br/>
+She puts her hand out for a handshake. I quickly shake her hands in an attempt to minimize all physical contact.<br/><br/>
+    "Ryan"<br/>
+    "I've heard so much about you! Ms. Stark was saying such great things!"<br/><br/>
+After a brief nod, I go and sit down in the very left corner and set up my notebook, pencils and everything else in a uniform sequence.<br/><br/>
+    "Oh, Ryan. You won't be needing that today."<br/>
+Seriously? What's the point of math class then? I just put everything back in my backpack and stick my hands in my pocket. I look around and watch lots of people arrive. Short, tall, boys, girls, each person more different than the next<br/>. After the national anthem, occasional first-day messages and attendance, we start with something unusual. <br/><br/>
+    "Today, rather than doing a lesson, we will be going over a few things to prepare for the rest of the semester, and then we will get to know each other"<br/>
+    What does she mean to get to know each other? Will I have to do anything? Ms. Miller goes to her desk and pulls up a slideshow on the projector. It's all about the importance of math and the course syllabus. I'm not even paying attention because I'm so worried about the supposed activity that she has planned. Like I said this morning, I don't want any surprises. Fifteen minutes later, the presentation finally ends.
+    "So I can see that all of you are bored. So let's do a quick activity."<br/><br/>
+Oh no.<br/><br/>
+    "We are just going to go around, say our names and one interesting thing about yourself. Feel free to raise your hand and participate."<br/><br/>
+She looks around, only to see a bored class.<br/>
+    "Anyone?!"<br/><br/>
+Finally, a girl on the other side of the class raises her hand.<br/>
+    "Thank you!"<br/>
+    "Hello. My name is Mia, and one interesting thing about me is that I have my own business."<br/>
+    "Woah! That's amazing! Thanks for participating. Anyone else?"<br/><br/>
+    More and more people find the confidence to speak out and participate. After each person goes, I try to memorize their names for future reference. Mia, Jeremiah, David, Cherry. Of course, the time finally came where no more people wanted to volunteer. Out of the blue, Ms. Miller looks at me. I know what's about to happen. <br/><br/>
+    "How about you, Ryan?"<br/>
+I shake my head quickly. <br/>
+    "Please?"<br/>
+Once again, I have submitted to the forces of authority. I give a brief nod and look at the class. No one told me how intimidating this was going to be. The eyes of over 20 kids just staring and intimidating you. <br/><br/>
+    "Hi. I'm Ryan."<br/>
+    "Could you tell us one thing about you?"<br/>
+I just sit there in silence, trying to think. Do I tell them about autism? Do I come up with something else?<br/>
+    "I have a dog."<br/>
+    "That's interesting! What's his name?"<br/>
+    "Dumpling"<br/>
+    "Nice name! Thank you so much for participating! In case all of you didn't know, Ryan has autism."<br/><br/>
+Why? Why would she tell them? I wanted to try and fit in. If people know, they are just going to treat me differently. Immediately, all the people looking at me staring for a few seconds and then directly looked away. Just great.<br/><br/>
+    "Alright. Anyone else wants to go?"
+The activity continues for another 15 minutes, only to be followed by another slideshow. Eventually, the bell rings, and everyone immediately gets up. The sounds of the chairs screeching across the floor, one at a time, were by far worse than the hallway music. I wait for everyone to get up before I do, just so that I have time to gather my thoughts and my things. Just before leaving, Ms. Miller cuts me off. <br/><br/>
+    "Thanks for participating today, Ryan."<br/><br/>
+I reply with a soft nod and head out the door. The hallways are much more peaceful now, given that students are far gone. According to my schedule, my next class is upstairs in room 201. It should be nice because I know the teacher from last semester. I don't think the students will be the same, but there might be one or two. After being extremely cautious during my trek towards the class, I finally arrive at the room. Today hasn't been so bad… right? 
+    Today's class is literally the same as the last. The same introduction, a very similar slideshow, another "fun" activity, the same closing remarks at the end and of course, the loud screeching of chairs. Bobby told me yesterday that the first day is always weird, but I don’t know how different it actually was. After class ended, I wait for everyone to leave before getting my things. However, this time, I decide to approach Mr. Brown. 
+    "Where do I go?"<br/><br/>
+    "Hi, Ryan. What do you mean?"<br/>
+    "During lunch. Where do I go?"<br/>
+    "To the cafeteria, or anywhere you wish for that matter."<br/>
+    "Where? Which room?"<br/>
+    "I'm not entirely sure, but I know it is next to the gym, which is room 105. Somewhere around there. Have a good lunch!"<br/><br/>
+    This should be a fun adventure. Finding a room without knowing where. After walking by the gym, I notice a sign indicating that the Cafeteria is close by. This is the loudest place I have ever been to. In every direction, you see and hear people running, laughing, screaming and eating loudly. I'm never going to complain about Dumpling being loud again. 
+    In the far left corner, I see an empty table that is perfect for me. I head on over, take a seat and bring out my lunchbox. <br/><br/>My dad packed me my favourite: a chicken sandwich, a bottle of water and a pack of my favourite chips. Before eating, I quickly put on some hand sanitizer and ensure that I don't touch anything more. At a decent pace, I eat my meal and finish it to completion. As simple as that. I look at the time: 11:50. How has it only been 20 minutes? I still have another 30 to spare. I knew this was going to happen, and so I brought my favourite book to give me comfort: The Great Gatsby. I've read this book at least 5 times, and each time is better than the last. 
+    Whenever I read, it takes me to another plant, where I explore, think and act in a different way.<br/><br/> For these 30 minutes, that is exactly where I went. Each page brought joy and comfort to my senses, so much so that I actually stopped hearing my surroundings. I was in the zone, lost in the work of a great author. After what felt like 5 minutes, the bell rings, and my feeling of relaxation is contrasted by the harsh reality of high school. I gather my things and head directly to my next class. 
+    English is my next class, and might I say this is not one of my favourites. So much speaking and interaction is involved; however, there is some reading, so that is a bonus. <br/><br/>This class starts off like the rest. The introduction, activity and announcement of my autism. Is it like they are trying to tell the entire world that I was born with Aspergers? It feels as if they want people to treat me differently. Anyways, we got a full syllabus of what is going to be covered and what is going to count for my final grade—the basic stuff. 
+    Ms. Patterson is really nice; she seems approachable and outgoing. Of course, not the type of person I aspire to be like, but I think she will make a really good teacher. Eventually, the bell rings, and I know what is coming. One by one, of course, the screech of the chair along the floor and then I continue with my day. However, my way to my final class is a little different. 
+    <br/><br/>Just as I am heading up the stairs, one of the boys from my class comes up to me and says to my face, "My mom said there would be a kid with autism in my class. Just so you know, I don't work with freaks". <br/><br/>
+    All of his friends start laughing and run up the stars. My heart… sinks. Someone saying something mean was inevitable. I prepared for it, and yet I didn't know how much it would hurt. It feels like a knife, just penetrating my gut. I don't feel like responding. Not only do I not know what to say, but I don't want to cause more trouble. Slowly, I head on over to my last class. I wanted to pay attention and hear what the course was, but all I could think about was what that kid said. Am I really a freak?
+    Eventually, I come to my senses, take a deep breath and decide to listen to what Mr. Garcia had to say. I think Political Science will be a course that I will surprisingly enjoy. <br/><br/>I thought it was going to be about debate, but it's more related to civil learning, which sounds pretty interesting. Just before the bell is about to ring, we hear an announcement on the overhead speakers.
+    "Attention all students. Thank you so much for an amazing day of learning! Please take a look at our school website for more information and have a great evening!"<br/><br/>
+    Eventually, the bell rings and we all head out the doors. Rather than heading down to the pickup area, I decided to go to the front office. 
+    "Hey there. What can I help you with?"<br/>
+    "Is Ms. Stark there?"<br/>
+    
+
+"Yeah. She just finished a meeting. Let me see if she is available."
              
               </p>
             </Col>
